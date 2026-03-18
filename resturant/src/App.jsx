52fe0734aa54dbeff2e43c8f6ab409919897b8c1
Sewar77 <sewarjihad793@gmail.com>
2026-03-18 14:41:46 +0300

    feat: add admin message management feature with approval and rejection functionality

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -8,7 +8,8 @@ import Login from "./Components/Auth/Login.jsx";
 import AdminDashboard from "./pages/Admin/AdminDashboard.jsx";
 import Dashboard from "./Components/User/Dashboard/Dashboard.jsx";
 import MenuManage from "./Components/Admin/Manage/MenuManage.jsx";
-import ContactUs from "./Components/User/Contact/ContactUs.jsx"
+import ContactUs from "./Components/User/Contact/ContactUs.jsx";
+import AdminManageMessages from "./pages/Admin/AdminManageMessages.jsx";
 function App() {
   return (
     <>
@@ -23,6 +24,7 @@ function App() {
         <Route path="/user/dashboard" element={<Dashboard />} />
         <Route path="/view-details" element={<ViewItem />} />
         <Route path="/messages" element={<ContactUs />} />
+        <Route path="/manage/messages" element={<AdminManageMessages />} />
       </Routes>
     </>
   );
